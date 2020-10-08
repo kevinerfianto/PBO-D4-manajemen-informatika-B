4b18461157e12ed55ae5ebe5ea944d96a1ebdf4e
kevinerfianto <61007506+kevinerfianto@users.noreply.github.com>
2020-10-08 21:44:45 +0700

    Add files via upload

---
+++ D4 RPL kevin044 Account.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+class Account
+{
+ public:
+  Account( int);
+  void credit(int );
+  void debit(int );
+  int getAccountBalance();
+ private:
+  int accountBalance;
+};
+Account::Account( int balance)
+{
+ if(balance>=0)
+  accountBalance=balance;
+ else
+ {
+  accountBalance=0;
+  cout<<"Initial balance was invalid."<<endl;
+ }
+}
+void Account::credit( int balance)
+{
+ accountBalance = accountBalance + balance;
+}
+void Account::debit( int balance)
+{
+ if(accountBalance>=balance)
+ {
+  accountBalance = accountBalance - balance;
+ }
+ else
+ {
+  cout<<"Debit amount exceeded account balance."<<endl;
+ }
+}
+int Account::getAccountBalance()
+{
+ return accountBalance;
+}
+int main()
+{
+	cout << "\t\t\t Account " <<endl <<endl;
+
+	cout << endl;
+	Account Account1(100000);
+	Account Account2(7500);
+	cout << "SALDO PERTAMA ACCOUNT 1 : Rp. "<< Account1.getAccountBalance() <<",-" <<endl;
+	cout << "SALDO PERTAMA ACCOUNT 2 : Rp. "<< Account2.getAccountBalance() <<",-" <<endl;
+	cout << endl;
+
+
+	cout << endl;
+	Account1.credit(50000);
+	Account2.credit(2500);
+	cout << "credit dari  ACCOUNT 1 : Rp. 5000 ,-" << endl;
+	cout << "credit dari  ACCOUNT 2 : Rp. 2500 ,-" << endl;
+
+	cout <<endl << "SETELAH CREDIT :" << endl <<endl;
+	cout << "Saldo ACCOUNT 1 : Rp. "<< Account1.getAccountBalance() <<",-" <<endl;
+	cout << "Saldo ACCOUNT 2 : Rp. "<< Account2.getAccountBalance() <<",-" <<endl;
+	cout << endl;
+
+
+	cout << endl;
+	Account1.debit(15000);
+	Account2.debit(500);
+	cout << "Debit ACCOUNT 1 : Rp. 1500 ,-" << endl;
+	cout << "Debit ACCOUNT 2 : Rp. 500 ,-" << endl;
+
+	cout <<endl << "SETELAH DEBIT :" << endl <<endl;
+	cout << "Saldo akhri dari ACCOUNT 1 : Rp. "<< Account1.getAccountBalance() <<",-"<< endl;
+	cout << "Saldo akhri dari ACCOUNT 2 : Rp. "<< Account2.getAccountBalance() <<",-"<< endl;
+
+}
